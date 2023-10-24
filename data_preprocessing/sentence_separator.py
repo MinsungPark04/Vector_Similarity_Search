23156c98c364d5adf6aacc49db9e97922b20c96a
luke <minsungpark04@gmail.com>
2023-10-24 23:40:17 +0900

    kss 문장 분리

---
+++ data_preprocessing/sentence_separator.py
@@ -0,0 +1,23 @@
+import kss
+import csv
+import pandas as pd
+
+def sentence_split(input_csv_path, output_csv_path):
+    dataframe = pd.read_csv(input_csv_path)
+
+
+    with open(output_csv_path, 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+
+            header = list(dataframe.columns) + ['kss_content']
+            
+            writer.writerow(header)
+
+            for index, row in dataframe.iterrows():
+
+                split_sentence = kss.split_sentences(row['content'])
+                
+                writer.writerow(list(row) + [str(split_sentence)])
+
+if __name__ == '__main__':
+    sentence_split(input_csv_path='data/refine_data.csv', output_csv_path='data/kss_data.csv')
